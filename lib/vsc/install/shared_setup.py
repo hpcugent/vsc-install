874a3d815d3a682d3a4432355361bee95c14c0f9
Jens Timmerman <jens.timmerman@ugent.be>
2018-01-25 13:09:51 +0100

    use newer pylint to avoid version conflicts
    issue with pylint and prospector was fixed, see https://github.com/landscapeio/prospector/issues/183

--- lib/vsc/install/shared_setup.py
+++ lib/vsc/install/shared_setup.py
@@ -1251,7 +1251,7 @@ class vsc_setup(object):
         else:
             log.info('adding prospector to tests_require')
             tests_requires = new_target.setdefault('tests_require', [])
-            tests_requires.extend(['prospector >= 0.12.1', 'pylint < 1.6.0'])
+            tests_requires.extend(['prospector >= 0.12.1', 'pylint'])
 
         # update the cmdclass with ones from vsc_setup_klass
         # cannot do this in one go, when SHARED_TARGET is defined, vsc_setup doesn't exist yet
